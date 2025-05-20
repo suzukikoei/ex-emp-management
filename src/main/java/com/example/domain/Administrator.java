59da4601e08a85103188dba005431237a213d405
suzukikoei <koeich6@gmail.com>
2025-05-20 11:00:54 +0900

    管理者ドメインを編集

--- src/main/java/com/example/domain/Administrator.java
+++ src/main/java/com/example/domain/Administrator.java
@@ -5,4 +5,68 @@ package com.example.domain;
  * */
 public class Administrator {
 
+    /** 管理者ID */
+    private Integer id;
+
+    /** 管理者の名前 */
+    private String name;
+
+    /** 管理者のメールアドレス */
+    private String mailAddress;
+
+    /** 管理者のパスワード */
+    private String password;
+
+    /** デフォルトコンストラクタ */
+    public Administrator(){}
+
+    /** コンストラクタ */
+    public Administrator(Integer id, String name, String mailAddress, String password) {
+        this.id = id;
+        this.name = name;
+        this.mailAddress = mailAddress;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mailAddress='" + mailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
